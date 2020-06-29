f8ad9e75a998a955d838d29359182ad1319952e7
rohit-b-kadam <rohitkadam1407@gmail.com>
2020-06-29 18:17:58 +0530

    update printing statement

--- C/simpleProgram.c
+++ C/simpleProgram.c
@@ -5,13 +5,14 @@
 void greeting( char* name)
 {
     printf("Hi, %s\n", name);    // 
-    printf("Welcome to World of C\n");
+    printf("Congrats you have successfully run the c program\n");
 }
 
 
 // Entry function of c
 int main()
 {
+    printf("Welcome to the C World\n");
     char name[20]; // declare a string variable to store a user name
     
     printf("Enter your name: ");    // display a message(Enter your name) on console 
